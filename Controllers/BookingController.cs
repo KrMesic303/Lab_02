7cd580b8414e7cf38977263d55990c034d69fbf4
Kristian Mesić <kristian.mesic303@gmail.com>
2023-10-31 21:06:25 +0100

    Add project files.

---
+++ Controllers/BookingController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Lab_02.Models;
+
+namespace Lab_02.Controllers
+{
+    public class BookingController : Controller
+    {
+        private static List<HotelBooking> hotelBookings = new();
+
+        public IActionResult Index()
+        {
+            return View(hotelBookings);
+        }
+
+        public IActionResult Create(int id = 0)
+        {
+            HotelBooking hotelBooking = new();
+
+            return View(hotelBooking);
+        }
+
+        public IActionResult CreateBooking(HotelBooking hotelBooking) 
+        {
+            hotelBooking.Id = hotelBookings.Count > 0 ? (hotelBookings.Max(x => x.Id) + 1) : 1;
+            hotelBookings.Add(hotelBooking);
+
+            return RedirectToAction("Create");
+        }
+    }
+}
---
+++ Models/HotelBooking.cs
@@ -0,0 +1,9 @@
+﻿namespace Lab_02.Models
+{
+    public class HotelBooking
+    {
+        public int Id { get; set; }
+        public string GuestName { get; set; } = string.Empty;
+        public int RoomNumber { get; set; }
+    }
+}
